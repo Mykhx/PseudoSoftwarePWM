62af4dc6afe3adfb18bb1ddebbbf70ec13e86117
mykhx <mkehry92@gmail.com>
2023-04-23 18:17:26 +0200

    adapted demo

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -2,9 +2,25 @@
 #include "gpiod.hpp"
 #include "pwm/PulseWidthModulator.h"
 #include "Demo.h"
+#include <random>
+
+using namespace std::chrono_literals;
 
 void modulateLine(PulseWidthModulator &pwm, const task& turnOn, const task& turnOff,
                   double increment, timePoint stopTime);
+
+void modulateLine3(PulseWidthModulator &pwm, const task& turnOn0, const task& turnOff0,double increment0,
+                   const task& turnOn1, const task& turnOff1,double increment1,
+                   const task& turnOn2, const task& turnOff2,double increment2,
+                   timePoint stopTime);
+
+void modulateLineRGBColor(PulseWidthModulator &pwm,
+                          const task &turnOn0, const task &turnOff0,
+                          const task &turnOn1, const task &turnOff1,
+                          const task &turnOn2, const task &turnOff2,
+                          timePoint stopTime);
+
+double getSubsequentIncrement(double increment, double currentPWMFraction);
 
 void demoGPIOPin17Modulation() {
     PulseWidthModulator pwm = PulseWidthModulator();
@@ -32,7 +48,7 @@ void demoGPIOPin17Modulation() {
 }
 
 void demoGPIOPin172722Modulation() {
-    PulseWidthModulator pwm = PulseWidthModulator();
+    PulseWidthModulator pwm = PulseWidthModulator(2000us);
 
     gpiod::chip chip("gpiochip0");
 
@@ -64,11 +80,28 @@ void demoGPIOPin172722Modulation() {
     turnOn27();
     turnOn22();
 
-    std::this_thread::sleep_for(2s);
+    std::this_thread::sleep_for(1s);
 
     turnOn17();
     turnOn27();
     turnOn22();
+
+    modulateLineRGBColor(pwm,
+                         turnOff17, turnOn17,
+                         turnOff22, turnOn22,
+                         turnOff27, turnOn27,
+                         timeProvider::now() + 60s);
+
+    std::this_thread::sleep_for(1s);
+
+    std::random_device random_device;
+    std::mt19937 random_engine(random_device());
+    std::uniform_int_distribution<int> distribution_2_8(2, 10);
+    modulateLine3(pwm,
+                  turnOff17, turnOn17, abs(static_cast<double>(distribution_2_8(random_engine))) / 100,
+                  turnOff27, turnOn27, abs(static_cast<double>(distribution_2_8(random_engine))) / 100,
+                  turnOff22, turnOn22, abs(static_cast<double>(distribution_2_8(random_engine))) / 100,
+                  timeProvider::now() + 60s);
 
     std::this_thread::sleep_for(1s);
 
@@ -76,13 +109,13 @@ void demoGPIOPin172722Modulation() {
 
     timePoint stopTime;
     std::cout << "Line: " << 17 << "\n";
-    modulateLine(pwm, turnOff17, turnOn17, increment, timeProvider::now() + 10s);
+    modulateLine(pwm, turnOff17, turnOn17, increment, timeProvider::now() + 5s);
 
     std::cout << "Line: " << 22 << "\n";
-    modulateLine(pwm, turnOff22, turnOn22, increment, timeProvider::now() + 10s);
+    modulateLine(pwm, turnOff22, turnOn22, increment, timeProvider::now() + 5s);
 
     std::cout << "Line: " << 27 << "\n";
-    modulateLine(pwm, turnOff27, turnOn27, increment, timeProvider::now() + 10s);
+    modulateLine(pwm, turnOff27, turnOn27, increment, timeProvider::now() + 5s);
 
     std::this_thread::sleep_for(1s);
 
@@ -93,7 +126,7 @@ void demoGPIOPin172722Modulation() {
 
 void modulateLine(PulseWidthModulator &pwm, const task& turnOn, const task& turnOff,
                   double increment, timePoint stopTime) {
-    double currentPWMFraction = 1.0;
+    double currentPWMFraction = 0.5;
     pwm.startPWM();
     while (timeProvider::now() < stopTime) {
         std::cout << "current increment " << currentPWMFraction << "\n";
@@ -106,6 +139,105 @@ void modulateLine(PulseWidthModulator &pwm, const task& turnOn, const task& turn
     }
     turnOff();
     pwm.stopPWM();
+
+    std::this_thread::sleep_for(1s);
+}
+
+void modulateLine3(PulseWidthModulator &pwm, const task &turnOn0, const task &turnOff0, double increment0,
+                   const task &turnOn1, const task &turnOff1, double increment1, const task &turnOn2,
+                   const task &turnOff2, double increment2, timePoint stopTime) {
+    double currentPWMFraction0 = 0.5;
+    double currentPWMFraction1 = 0.5;
+    double currentPWMFraction2 = 0.5;
+
+    pwm.startPWM();
+    while (timeProvider::now() < stopTime) {
+        std::cout << "current increments "
+                  << currentPWMFraction0 << " "
+                  << currentPWMFraction1 << " "
+                  << currentPWMFraction2 << "\n";
+
+        pwm.registerTask(turnOff0, turnOn0, timeProvider::now(), currentPWMFraction0);
+        pwm.registerTask(turnOff1, turnOn1, timeProvider::now(), currentPWMFraction1);
+        pwm.registerTask(turnOff2, turnOn2, timeProvider::now(), currentPWMFraction2);
+
+        std::this_thread::sleep_for(5s);
+
+
+        increment0 = getSubsequentIncrement(increment0, currentPWMFraction0);
+        increment1 = getSubsequentIncrement(increment1, currentPWMFraction1);
+        increment2 = getSubsequentIncrement(increment2, currentPWMFraction2);
+
+        currentPWMFraction0 -= increment0;
+        currentPWMFraction1 -= increment1;
+        currentPWMFraction2 -= increment2;
+
+        pwm.clearAllTasks();
+        turnOn0();
+        turnOn1();
+        turnOn2();
+    }
+    pwm.stopPWM();
+
+    turnOn0();
+    turnOn1();
+    turnOn2();
+
+    std::this_thread::sleep_for(1s);
+}
+
+double getSubsequentIncrement(double increment, double currentPWMFraction) {
+    if (currentPWMFraction <= abs(increment)
+    or currentPWMFraction >= 1 - abs(increment)
+    or currentPWMFraction <= 0.2
+    or currentPWMFraction >= 0.9)
+        increment = -increment;
+    return increment;
+}
+
+void modulateLineRGBColor(PulseWidthModulator &pwm, const task &turnOn0, const task &turnOff0, const task &turnOn1,
+                          const task &turnOff1, const task &turnOn2, const task &turnOff2, timePoint stopTime) {
+
+    std::random_device random_device;
+    std::mt19937 random_engine(random_device());
+    std::uniform_int_distribution<int> distribution(1, 99);
+
+    PulseWidthModulator pwm0 = PulseWidthModulator(3000us);
+    PulseWidthModulator pwm1 = PulseWidthModulator(3000us);
+    PulseWidthModulator pwm2 = PulseWidthModulator(3000us);
+    pwm0.startPWM();
+    pwm1.startPWM();
+    pwm2.startPWM();
+    while (timeProvider::now() < stopTime) {
+        double currentPWMFraction0 = abs(static_cast<double>(distribution(random_engine))) / 100;
+        double currentPWMFraction1 = abs(static_cast<double>(distribution(random_engine))) / 100;
+        double currentPWMFraction2 = abs(static_cast<double>(distribution(random_engine))) / 100;
+
+        std::cout << "current fractions "
+                  << currentPWMFraction0 << " "
+                  << currentPWMFraction1 << " "
+                  << currentPWMFraction2 << "\n";
+
+        auto firstExecution = timeProvider::now();
+
+        pwm0.registerTask(turnOff0, turnOn0, firstExecution, currentPWMFraction0);
+        pwm1.registerTask(turnOff1, turnOn1, firstExecution, currentPWMFraction1);
+        pwm2.registerTask(turnOff2, turnOn2, firstExecution, currentPWMFraction2);
+
+
+        std::this_thread::sleep_for(1s);
+
+        pwm0.clearAllTasks();
+        pwm1.clearAllTasks();
+        pwm2.clearAllTasks();
+    }
+    pwm0.stopPWM();
+    pwm1.stopPWM();
+    pwm2.stopPWM();
+
+    turnOn0();
+    turnOn1();
+    turnOn2();
 
     std::this_thread::sleep_for(1s);
 }
